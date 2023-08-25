5b56b12e9117e6cf70e0ee0c1836b764f85de658
Michel Sabchuk <michel@turbosys.com.br>
2023-08-25 19:49:41 -0300

    Wip

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import './style.css';
 
 const style = {
   bg: '#241c1c',
+  wall: 'rgb(108,83,83)',
+  l: 'rgba(255,0,255,0.3)',
 }
 
-function getWalls() {
-  const bg = `<rect width="1600" height="1100" style="fill:rgb(108,83,83);" />`;
+function getWalls(_width: number, height: number) {
+  const bg = `<rect width="1600" height="${height - 100}" style="fill:${style.wall};" />`;
   const floorPerspective = `
     <polygon
-      points="-2,952 -2,1102 500,1102"
+      points="-2,${height - 148} -2,${height - 98} 500,${height - 98}"
       style="fill:${style.bg}"
     />
   `;
@@ -18,15 +20,28 @@ function getWalls() {
       style="fill:${style.bg}"
     />
   `;
+  const internalFloor = `
+    <polygon
+      points="0,50 160,0 420,0 580,50"
+      style="fill:${style.bg}"
+    />
+  `;
   return `
-    <svg style="height: 100%" viewBox="0 0 1600 1200"  preserveAspectRatio="true">
+    <svg width="1600" height="${height}" style="width: 1600px; height: ${height}px" viewBox="0 0 1600 ${height}" preserveAspectRatio="true">
       ${bg}
       ${floorPerspective}
       ${mirror(floorPerspective)}
       ${roofPespective}
       ${mirror(roofPespective)}
       ${pillar(480, 50)}
-      ${pillar(1600 - 500, 50)}
+      ${pillar(1600 - 520, 50)}
+      ${move(roofPespective, 601, 0)}
+      ${move(mirror(roofPespective), -601, 0)}
+      <rect x="680" y="91" width="240" height="50" style="fill:${style.bg};" />
+      <rect x="500" y="141" width="580" height="480" style="fill:${style.wall};" />
+      ${pillar(660, 92)}
+      ${pillar(1600 - 700, 92)}
+      ${move(internalFloor, 500, 617)}
     </svg>
   `;
 }
@@ -43,12 +58,18 @@ function mirror(content: string) {
   `
 }
 
+function move(content: string, x: number, y: number) {
+  return `
+    <g transform="translate(${x},${y})">${content}</g>
+  `
+}
+
 function runApp() {
   const root = document.querySelector<HTMLDivElement>('#app');
   if(!root) {
     return
   }
-  root.innerHTML = getWalls();
+  root.innerHTML = getWalls(window.innerWidth, window.innerHeight);
 }
 
 runApp();
