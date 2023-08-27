bd44b0f6335e23669183317af36807f8f0e21314
Michel Sabchuk <michel@turbosys.com.br>
2023-08-26 21:10:36 -0300

    Adjust shadow effect to be compatible with chrome

--- src/shadow.ts
+++ src/shadow.ts
@@ -1,21 +1,21 @@
 import {theme} from './theme';
 import {rect, move} from './utils';
 
-const shadow = rect(920, 2800, "url('#shadow')");
+const shadow = rect(800, 2800, "url('#shadow')");
 
 export function createShadow() {
   return `<svg width="720" height="900" viewBox"0 0 720 900">
     <defs>
       <radialGradient fr="20%" id="shadow">
-        <stop offset="0%" stop-color="rgba(0,0,0,0)" />
-        <stop offset="66%" stop-color="${theme.bg}">
-          <animate attributeName="offset" dur="3s" values="66%;60%;66%;60%;66%" repeatCount="indefinite" />
+        <stop offset="0%" stop-color="rgba(0,0,0,0)">
+          <animate attributeName="stop-color" dur="500ms" values="${theme.bg0};${theme.bg5}" repeatCount="indefinite" />
         </stop>
+        <stop offset="75%" stop-color="${theme.bg}">
         <stop offset="100%" stop-color="${theme.bg}" />
-        <animate attributeName="fx" dur="10s" values="50%;48%;50%;52%;50%" repeatCount="indefinite" />
-        <animate attributeName="fy" dur="10s" begin="2s" values="50%;45%;50%;55%;50%" repeatCount="indefinite" />
+        <animate attributeName="fx" dur="5s" values="50%;47%;50%;53%" repeatCount="indefinite" />
+        <animate attributeName="fy" dur="5s" begin="2s" values="50%;42%;50%;57%" repeatCount="indefinite" />
       </radialGradient>
     </defs>
-    ${move(shadow, -100, -1000)}
+    ${move(shadow, -50, -1000)}
   </svg>`;
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,7 @@
 export const theme = {
-  bg: '#241c1c',
+  bg: 'rgb(36,28,28)',
+  bg0: 'rgba(36,28,28,0)',
+  bg5: 'rgba(36,28,28,0.02)',
   wall: 'rgb(108,83,83)',
   brick: 'rgba(0,0,0,0.1)',
   l: 'rgba(255,0,255,0.3)',
