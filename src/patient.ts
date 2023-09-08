4121f606dd817efd5713fa18ff4f769dde271bdf
Michel Sabchuk <michel@turbosys.com.br>
2023-09-08 09:07:14 -0300

    Add first character

--- src/patient.ts
+++ src/patient.ts
@@ -4,6 +4,7 @@ import {
   rect,
   ellipsis,
   move,
+  resize,
   rotate,
   hmirror,
   blur,
@@ -26,10 +27,10 @@ export function createPatient(container: HTMLElement) {
   setTimeout(() => {
     getElement("pnt");
   });
-  const objWidth = 110;
-  const objHeight = 170;
+  const objWidth = 121;
+  const objHeight = 141;
   container.innerHTML = wrapper(
-    group(Health.Dead) + getTakenMedicine(patient),
+    char1(Health.Good) + getTakenMedicine(patient),
     objWidth,
     objHeight,
     {
@@ -45,7 +46,7 @@ const opacity = move(rect(94, 94, theme.white(0.3), 35), 6, 0);
 
 const eye = ellipsis(5, 5, 5, 5, theme.black());
 
-const eyeDecoration = `<path d="m2.2 0.5c-1.9 1.8-2.6 4.9 0 8.3" fill="none" stroke="${theme.black()}" stroke-width="2" stroke-linecap="round"/>`;
+const eyeDecoration = `<path d="m2.2 0.5c-1.9 1.8-2.6 4.9 0 8.3" fill="none" stroke="${theme.black()}" stroke-width="1.5" stroke-linecap="round"/>`;
 
 const deadEyeLine = (direction: 1 | -1) =>
   move(rotate(rect(16, 2, theme.black()), direction * 45), direction * -5, 5);
@@ -56,18 +57,31 @@ const face = (health: Health) =>
   [
     back,
     opacity,
-    health !== Health.Dead ? move(eye, 20, 30) + move(eye, 50, 30) : "",
+    health !== Health.Dead ? move(eye, 10, 40) + move(eye, 40, 40) : "",
     health === Health.Bad
-      ? move(eyeDecoration, 15, 30) + hmirror(move(eyeDecoration, 35, 30))
+      ? move(eyeDecoration, 6, 40) + hmirror(move(eyeDecoration, 52, 40))
       : "",
     health === Health.Dead
-      ? move(deadEye, 17.5, 26.5) + move(deadEye, 47.5, 26.5)
-      : "", // instead of 20, 30 and 50, 30
+      ? move(deadEye, 7.5, 36.5) + move(deadEye, 37.5, 36.5)
+      : "", // instead of orig eye's position
   ].join("");
 
 const shadow = blur(move(rect(94, 94, theme.black(0.3), 35), 2, 8), 4);
 
-const group = (health: Health) => wrapper(shadow + move(face(health), 6, 0), 106, 106);
+const hair = move(
+  resize(
+    `<path d="m18.1 33.6c4.48-14.8 3.53-19.5-3.5-20.7-2.17 6.77-12.5 5.61-14.3 2.88-0.076-6.38 1.08-14.9 12-15.5 3.6-0.191 7.65 1.39 9.38 5.54 7.97-0.938 8.67 20.6 1.78 27.4z" fill="#fc0" stroke="#000" stroke-linejoin="round" stroke-width=".606"/>`,
+    4,
+    4,
+  ),
+  0,
+  0,
+);
+
+const group = (health: Health) =>
+  wrapper(shadow + move(face(health), 6, 0), 106, 106);
+
+const char1 = (health: Health) => wrapper(move(group(health), 0, 35) + move(hair, 7, 0), 121, 141);
 
 function getTakenMedicine(patient: Patient) {
   setTimeout(() => {
