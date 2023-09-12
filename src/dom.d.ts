8dad2b8ca3c6bb6b3a5fb493f7a321e79f93be01
Michel Sabchuk <michel@turbosys.com.br>
2023-09-12 11:21:55 -0300

    Declare window

--- src/dom.d.ts
+++ src/dom.d.ts
@@ -1,20 +1,28 @@
-interface GlobalEventHandlersEventMap {
-  reset: CustomEvent;
-  notify: CustomEvent<{
-    messages: string[];
-    onDismissed?: () => void;
-    preventDismiss?: boolean;
-  }>;
-  dismiss: CustomEvent;
-  newLevel: CustomEvent<{ level: Level }>;
-  scoreMaxed: CustomEvent;
-  levelComplete: CustomEvent;
-  potionClick: CustomEvent<{ ingredient: Ingredient }>;
-  potionRelease: CustomEvent<{ ingredient: Ingredient }>;
-  cauldronDrop: CustomEvent<{ ingredient: Ingredient }>;
-  cauldronPrepared: CustomEvent<{ ingredient: Ingredient }>;
-  patientCalled: CustomEvent<{ patient: Patient }>;
-  patientDone: CustomEvent<{ patient: Patient }>;
-  patientLeave: CustomEvent<{ patient: Patient }>;
-  gameOver: CustomEvent;
+import type { Level, Ingredient, Patient } from './data';
+
+declare global {
+  interface Window {
+    scale: number;
+  }
+
+  interface GlobalEventHandlersEventMap {
+    reset: CustomEvent;
+    notify: CustomEvent<{
+      messages: string[];
+      onDismissed?: () => void;
+      preventDismiss?: boolean;
+    }>;
+    dismiss: CustomEvent;
+    newLevel: CustomEvent<{ level: Level }>;
+    scoreMaxed: CustomEvent;
+    levelComplete: CustomEvent;
+    potionClick: CustomEvent<{ ingredient: Ingredient }>;
+    potionRelease: CustomEvent<{ ingredient: Ingredient }>;
+    cauldronDrop: CustomEvent<{ ingredient: Ingredient }>;
+    cauldronPrepared: CustomEvent<{ ingredient: Ingredient }>;
+    patientCalled: CustomEvent<{ patient: Patient }>;
+    patientDone: CustomEvent<{ patient: Patient }>;
+    patientLeave: CustomEvent<{ patient: Patient }>;
+    gameOver: CustomEvent;
+  }
 }
